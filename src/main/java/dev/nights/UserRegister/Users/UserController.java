9ce0a159b752eb050b9fb6b2f26dd96577d3c33b
n1ghts <lucasbbastos18@gmail.com>
2025-08-27 19:12:22 -0300

    feat: added endpoints to the API

--- src/main/java/dev/nights/UserRegister/Users/UserController.java
+++ src/main/java/dev/nights/UserRegister/Users/UserController.java
@@ -1,15 +1,35 @@
 package dev.nights.UserRegister.Users;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
 @RequestMapping
 public class UserController {
+
     @GetMapping("/welcomeUser")
     public String welcome(){
         return "Hello, user!";
+    }
+
+    //Register user
+    @PostMapping("/register")
+        public String userRegister(){
+            return "User registered";
+        }
+    //Show users registered
+    @GetMapping("/show")
+        public String showUsers(){
+        return "Users:";
+    }
+    //Update user by ID
+    @PutMapping("/update")
+        public String updateUsers(){
+        return "User updated";
+    }
+    //Delete user by ID
+    @DeleteMapping("/delete")
+        public String deleteUser(){
+        return "User deleted.";
     }
 }
